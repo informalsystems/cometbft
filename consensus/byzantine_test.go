03c2bae89b5dbf75a27bb8e0443635d545e984fe
Jasmina Malicevic <jasmina.dustinac@gmail.com>
2025-05-16 18:32:19 +0200

    Fix UT to use blobs and set maxBlobBytes to 800kb

--- consensus/byzantine_test.go
+++ consensus/byzantine_test.go
@@ -304,7 +304,7 @@ func TestByzantineConflictingProposalsWithPartition(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	app := newKVStore
+	app := newKVStoreWithBlob
 	css, cleanup := randConsensusNet(t, N, "consensus_byzantine_test", newMockTickerFunc(false), app)
 	defer cleanup()
 
@@ -353,6 +353,7 @@ func TestByzantineConflictingProposalsWithPartition(t *testing.T) {
 		require.NoError(t, err)
 
 		conR := NewReactor(css[i], true) // so we don't start the consensus states
+		conR.conS.state.ConsensusParams.Blob.MaxBytes = types.MaxBlobSizeBytes
 		conR.SetLogger(logger.With("validator", i))
 		conR.SetEventBus(eventBus)
 
